bd09a09dc8de592a45d8cdfe190c10fb91fa8ec9
xiongneng <yidao620@gmail.com>
2022-03-17 23:30:33 +0800

    update leetcode 19 and 876

---
+++ algorithms/leecode/a19_remove_nth_node_from_end_of_list.py
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+"""
+19. 删除单链表的倒数第N个结点
+
+给你一个单链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+
+两轮循环：
+1、第一轮循环获取链表的节点总数size。
+2、第二轮循环删除第size-n位置上的节点。
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return str(self.val)
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        if not head:
+            return head
+        size = 0
+        temp = head
+        while temp:
+            size += 1
+            temp = temp.next
+        if n > size:
+            return head
+
+        pre = None
+        remove_item = head
+        for _ in range(size - n):
+            pre = remove_item
+            remove_item = remove_item.next
+        if pre:
+            pre.next = remove_item.next
+        else:
+            head = remove_item.next
+        return head
+
+
+if __name__ == '__main__':
+    # 本地运行需要以下输入输出的补充
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+
+    # solve
+    solution = Solution()
+    res = solution.removeNthFromEnd(head, 5)
+    print()
+    # output
+
+
---
+++ algorithms/leecode/a876_middle_of_the_linked_list.py
@@ -0,0 +1,68 @@
+# -*- encoding: utf-8 -*-
+"""
+876. 链表的中间结点
+给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+
+如果有两个中间结点，则返回第二个中间结点。
+
+示例 1：
+输入：[1,2,3,4,5]
+输出：此列表中的结点 3 (序列化形式：[3,4,5])
+返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+
+示例 2：
+输入：[1,2,3,4,5,6]
+输出：此列表中的结点 4 (序列化形式：[4,5,6])
+由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+
+快慢指针经典案例。
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return str(self.val)
+
+    def __iter__(self):
+        node = self
+        while node:
+            yield node.val
+            node = node.next
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        if fast.next:  # 偶数情况
+            return slow.next
+        else:
+            return slow
+
+
+if __name__ == '__main__':
+    # input
+    # list_input = list(input('input list: ').split())
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+    head.next.next.next.next.next = ListNode(6)
+    # solve
+    solution = Solution()
+    res = solution.middleNode(head)
+    # output
+    print(list(res))
+
+
