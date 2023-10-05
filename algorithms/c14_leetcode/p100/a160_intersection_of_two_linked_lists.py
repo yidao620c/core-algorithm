9f4bb92b7141bae73613e615a5cc3f4d1752829c
xiongneng <yidao620@gmail.com>
2023-10-06 00:12:35 +0800

    update

---
+++ algorithms/c14_leetcode/p100/a160_intersection_of_two_linked_lists.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+"""
+160. 相交链表
+给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+输出：Intersected at '8'
+输入：intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+输出：Intersected at '2'
+
+解题思路：
+我们可以让 p1 遍历完链表 A 之后开始遍历链表 B，让 p2 遍历完链表 B 之后开始遍历链表 A，
+这样相当于「逻辑上」两条链表接在了一起。
+如果这样进行拼接，就可以让 p1 和 p2 同时进入公共部分，也就是同时到达相交节点 c1
+"""
+from typing import Optional
+
+from algorithms.c01_linear.linkedlist import ListNode
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        p1 = headA
+        p1_change = False
+        p2 = headB
+        p2_change = False
+        while p1 != p2:
+            if p1.next:
+                p1 = p1.next
+            elif not p1_change:
+                p1 = headB
+                p1_change = True
+            else:
+                p1 = None
+            if p2.next:
+                p2 = p2.next
+            elif not p2_change:
+                p2 = headA
+                p2_change = True
+            else:
+                p2 = None
+        return p1
